55fa8d9ff712c82f9fb1afe637ac166d70150907
Blaine Myers <bmyers@beemsoft.com>
2020-10-20 22:00:39 -0500

    [EXPLOR] Remove temp source file.

--- games/Explor2002/Source_Old/Beta 1/2020/BUILD.bat
+++ games/Explor2002/Source_Old/Beta 1/2020/BUILD.bat
@@ -1,10 +1,11 @@
 @echo off
 set PATH=C:\QB45;%PATH%
-echo Compiling graphics.bas
+echo Compiling graphics.bas...
 bc /o graphics.bas graphics.obj ;
-echo Compling walls.bas
+echo Compling walls.bas...
 bc /o walls.bas walls.obj ;
+echo Compiling walls2.bas...
 bc /o walls2.bas walls2.obj ;
-echo Compling main.bas
+echo Compling main.bas...
 bc /o main.bas main.obj ;
 link /nod main.obj walls2.obj graphics.obj, EXPLOR.exe, EXPLOR.map, C:\qb45\lib\bcom45.lib
